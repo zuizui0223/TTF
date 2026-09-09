61de563c26350394ef83d9330aa1178700a55b64
ZHANG RUIQI <rachelzhang0223@gmail.com>
2026-09-09 12:38:13 +0900

    Export TTF-M paired-state API

--- src/ttf/__init__.py
+++ src/ttf/__init__.py
@@ -1,8 +1,7 @@
 """Transferable Turnover Fields (TTF).
 
-A cross-taxon method for testing whether within-species trait-transition
-structure learned from some species predicts transition structure in unseen
-species.
+A cross-system method for testing whether within-system transition structure
+learned from some systems predicts transition structure in unseen systems.
 """
 
 from .atlas import RecurrenceEstimate, recurrence_probability
@@ -37,6 +36,17 @@ from .inference import (
     centered_species_bootstrap_mean_test,
     heldout_species_bootstrap_test,
 )
+from .mismatch import (
+    PairedSpeciesSample,
+    build_coupling_edges,
+    build_mismatch_edges,
+    default_mismatch,
+    default_relative_state,
+    mismatch_species_sample,
+    pointwise_mismatch,
+    pointwise_relative_state,
+    relative_species_sample,
+)
 from .nulls import (
     PermutationResult,
     fixed_graphs,
@@ -66,6 +76,7 @@ __all__ = [
     "GeometrySyntheticWorld",
     "KernelBoundaryModel",
     "MeanBootstrapResult",
+    "PairedSpeciesSample",
     "PermutationResult",
     "PredictorCompetitionResult",
     "PredictorTransferResult",
@@ -79,22 +90,30 @@ __all__ = [
     "SyntheticWorld",
     "TransferResult",
     "balanced_schedule",
+    "build_coupling_edges",
+    "build_mismatch_edges",
     "build_species_edges",
     "centered_species_bootstrap_mean_test",
     "edge_turnover",
     "compete_predictor_spaces",
+    "default_mismatch",
+    "default_relative_state",
     "fit_boundary_model",
     "fixed_graphs",
     "geometry_fingerprint",
     "heldout_species_bootstrap_test",
     "inclusion_counts",
     "knn_edges",
+    "mismatch_species_sample",
     "permutation_test",
     "permute_trait_within_species",
+    "pointwise_mismatch",
+    "pointwise_relative_state",
     "prepare_transfer",
     "qualify_calibration",
     "rank01",
     "recurrence_probability",
+    "relative_species_sample",
     "ridge_predictor_transfer",
     "run_calibration",
     "run_geometry_calibration",
@@ -105,4 +124,4 @@ __all__ = [
     "transfer_statistic",
 ]
 
-__version__ = "0.2.0"
+__version__ = "0.3.0.dev0"
